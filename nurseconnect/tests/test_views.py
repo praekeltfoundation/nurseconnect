4264cd563494b8c46f09aa0e126ed9617c449b4a
chrislombaard <chrislombaard@gmail.com>
2016-10-21 12:01:51 +0200

    Re-added tests for clinic code.

--- nurseconnect/tests/test_views.py
+++ nurseconnect/tests/test_views.py
@@ -1,11 +1,11 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase, Client
 from molo.core.tests.base import MoloTestCaseMixin
-# from molo.profiles.models import SecurityQuestion
+from molo.profiles.models import SecurityQuestion
 
 from nurseconnect import forms
-# from wagtail.contrib.settings.context_processors import SettingsProxy
-# from wagtail.wagtailcore.models import Site
+from wagtail.contrib.settings.context_processors import SettingsProxy
+from wagtail.wagtailcore.models import Site
 
 
 class RegistrationViewTest(MoloTestCaseMixin, TestCase):
@@ -52,35 +52,35 @@ class RegistrationViewTest(MoloTestCaseMixin, TestCase):
                 "This field is required."
             ])
 
-    # def test_security_questions(self):
-    #     site = Site.objects.get(is_default_site=True)
-    #     settings = SettingsProxy(site)
-    #
-    #     SecurityQuestion.objects.create(
-    #         title="What is your name?",
-    #         slug="what-is-your-name",
-    #         path="0002",
-    #         depth=1,
-    #     )
-    #
-    #     profile_settings = settings['profiles']['UserProfilesSettings']
-    #     profile_settings.show_security_question_fields = True
-    #     profile_settings.security_questions_required = True
-    #     profile_settings.save()
-    #
-    #     response = self.client.get(
-    #           reverse('user_register_security_questions')
-    #     )
-    #     self.assertContains(response, "What is your name")
-    #
-    #     # register with security questions
-    #     response = self.client.post(
-    #         reverse("user_register_security_questions"),
-    #         {},
-    #     )
-    #     self.assertFormError(
-    #         response, "form", "question_0",
-    #         ["This field is required."])
+    def test_security_questions(self):
+        site = Site.objects.get(is_default_site=True)
+        settings = SettingsProxy(site)
+
+        SecurityQuestion.objects.create(
+            title="What is your name?",
+            slug="what-is-your-name",
+            path="0002",
+            depth=1,
+        )
+
+        profile_settings = settings['profiles']['UserProfilesSettings']
+        profile_settings.show_security_question_fields = True
+        profile_settings.security_questions_required = True
+        profile_settings.save()
+
+        response = self.client.get(
+              reverse('user_register_security_questions')
+        )
+        self.assertContains(response, "What is your name")
+
+        # register with security questions
+        response = self.client.post(
+            reverse("user_register_security_questions"),
+            {},
+        )
+        self.assertFormError(
+            response, "form", "question_0",
+            ["This field is required."])
 
     def test_register_clinic_code_view_invalid_form(self):
         # NOTE: empty form submission
