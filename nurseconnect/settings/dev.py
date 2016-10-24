c1ff0e235611b6a93c21587cf7f08c666445d8d9
chrislombaard <chrislombaard@gmail.com>
2016-10-24 14:22:29 +0200

    Removed dev Clinic code api

--- nurseconnect/settings/dev.py
+++ nurseconnect/settings/dev.py
@@ -15,10 +15,6 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 #     },
 # }
 
-# # For dev, REMOVE
-# CLINIC_CODE_API = "http://praekelt:praekelt@npr-staging.jembi.org:5001/ws/" \
-#                   "rest/v1/NCfacilityCheck?"
-
 try:
     from .local import *  # noqa
 except ImportError:
