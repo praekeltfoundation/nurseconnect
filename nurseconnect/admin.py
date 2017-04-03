19d22c10a6f20ec4799388a0eabb7803a23ced6c
nathanbegbie <nathanbegbie@gmail.com>
2017-04-03 12:55:59 +0200

    Remove custom wagtail admin for yourwords

--- nurseconnect/admin.py
+++ nurseconnect/admin.py
@@ -7,11 +7,6 @@ from django.http import HttpResponse
 
 from molo.core.models import ArticlePage
 from molo.profiles.admin import FrontendUsersModelAdmin, ProfileUserAdmin
-from molo.yourwords.admin import (
-    YourWordsCompetitionAdmin, YourWordsCompetitionEntryAdmin)
-from molo.yourwords.models import (
-    YourWordsCompetition, YourWordsCompetitionEntry)
-from wagtailmodeladmin.options import ModelAdminGroup
 from wagtailmodeladmin.options import ModelAdmin as WagtailModelAdmin
 
 from nurseconnect.admin_views import NurseConnectFrontendUsersAdminView
@@ -50,34 +45,6 @@ class ArticlePageModelAdmin(WagtailModelAdmin):
             feature_as_topic_of_the_day=True
         ).order_by("-promote_date")
         return queryset
-
-
-class MoloYourWordsCompetitionModelAdmin(
-        WagtailModelAdmin, YourWordsCompetitionAdmin):
-
-    model = YourWordsCompetition
-
-    list_display = ("entries", "start_date", "end_date", "status",
-                    "number_of_entries")
-    list_filter = ("title", "start_date", "end_date")
-    search_fields = ("title", "content", "description")
-
-
-class MoloYourWordsCompetitionEntryModelAdmin(
-        WagtailModelAdmin, YourWordsCompetitionEntryAdmin):
-
-    model = YourWordsCompetitionEntry
-    list_display = ("story_name", "truncate_text", "user", "hide_real_name",
-                    "submission_date", "is_read", "is_shortlisted",
-                    "is_winner", "_convert")
-
-
-class YourWordsModelAdminGroup(ModelAdminGroup):
-    menu_label = "Your Words"
-    menu_icon = "edit"
-    menu_order = 300
-    items = (MoloYourWordsCompetitionModelAdmin,
-             MoloYourWordsCompetitionEntryModelAdmin)
 
 
 def download_as_csv(NurseConnectEndUsersModelAdmin, request, queryset):
