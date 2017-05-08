d60411fe710534f435ad8d43caf397240cb40d55
ruands <ruan.desousa@praekeltconsulting.com>
2017-05-08 15:59:49 +0200

    Change validation to show the correct error at the appropriate times

--- nurseconnect/forms.py
+++ nurseconnect/forms.py
@@ -84,14 +84,20 @@ class RegistrationMSISDNForm(forms.Form):
         label=_("Accept the Terms of Use")
     )
 
+    def clean_password(self):
+        if not self.cleaned_data.get("password", None):
+            raise forms.ValidationError()
+        return self.cleaned_data.get("password", None)
+
+    def clean_confirm_password(self):
+        if not self.cleaned_data.get("confirm_password", None):
+            raise forms.ValidationError()
+        return self.cleaned_data.get("confirm_password", None)
+
     def clean(self):
-        password = self.cleaned_data.get("password", None)
-        confirm_password = self.cleaned_data.get("confirm_password", None)
-        if (password and confirm_password and
-                (password == confirm_password)):
-            return self.cleaned_data
-        else:
-            raise forms.ValidationError(_("Passwords do not match."))
+        if not (self.cleaned_data.get("password") ==
+            self.cleaned_data.get("confirm_password")):
+            raise forms.ValidationError(_("Passwords do not match"))
 
 
 class RegistrationSecurityQuestionsForm(forms.Form):
