0fe071692ba77f2177bb0e502d4be62c0dc63828
Codie Roelf <codie@praekelt.com>
2017-08-15 18:05:22 +0200

    remove stray prints

--- nurseconnect/wagtail_hooks.py
+++ nurseconnect/wagtail_hooks.py
@@ -29,7 +29,6 @@ class ArticlePageModelAdmin(ModelAdmin):
     list_display = ("title", "promote_date", "demote_date")
 
     def get_queryset(self, request):
-        print 'in here'
         # Return only Topic of the day articles
         queryset = ArticlePage.objects.filter(
             feature_as_topic_of_the_day=True
