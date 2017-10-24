dff4b7453af26b80b5878218a7d848ec579d2e05
Mitso <mqalaba@gmail.com>
2017-10-24 12:39:25 +0200

    remove unused style

--- nurseconnect/static/src/js/main.js
+++ nurseconnect/static/src/js/main.js
@@ -6,34 +6,32 @@
 */
 (function() {
 
-  var searchLink = document.getElementById('NavBar__search'),
+    var searchLink = document.getElementById('NavBar__search'),
       searchFormField = document.getElementById('search');
       searchLink.addEventListener('click', function() {
         searchFormField.focus();
       });
 
   //SET COOKIE
-  function createCookie(name, value, days) {
+    function createCookie(name, value, days) {
     var expires;
     if(days) {
       var date = new Date();
       date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
       expires = "; expires=" + date.toGMTString();
-    } else {
-      expires = "";
+    } else { expires = ""; }
+        document.cookie = name + "=" + value + expires + "; path=/";
     }
-    document.cookie = name + "=" + value + expires + "; path=/";
-  }
 
-  //READ COOKIE - WHEN USER FIRST ARRIVES
-  function readCookie(c_name) {
+    //READ COOKIE - WHEN USER FIRST ARRIVES
+    function readCookie(c_name) {
     var value = "; " + document.cookie;
     var parts = value.split(";" + c_name + "=");
     if(parts.length == 2)
-    return parts.pop().split(";").shift();
-  }
+        return parts.pop().split(";").shift();
+    }
 
-  //SET COOKIE VALUE
-  createCookie("banner_image", 1 , 30);
-  var get_cookieValue = readCookie("banner_image")
+    //SET COOKIE VALUE
+    createCookie("banner_image", 1 , 30);
+    var get_cookieValue = readCookie("banner_image")
 })();
