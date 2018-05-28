45e1f05b120e275d375515e821bc359325c65db5
nathanbegbie <nathanbegbie@gmail.com>
2018-05-28 22:22:59 +0200

    Ensure blank rather than null values for AnalyticsRecord

--- analytics/utils.py
+++ analytics/utils.py
@@ -41,13 +41,13 @@ def get_tracking_params(request, response, timestamp=None, visitor_uuid=None):
     '''
     return({
         "timestamp": timestamp or timezone.now(),
-        "path": request.get_full_path(),
+        "path": request.get_full_path() or '',
         "referer": request.META.get('HTTP_REFERER', request.GET.get('r', '')),
         "investigation_uuid": request.COOKIES.get('investigation_uuid', ''),
         "domain": request.META.get('HTTP_HOST', ''),
         "user_agent": request.META.get('HTTP_USER_AGENT', 'Unknown'),
-        "title": get_response_title(response),
-        "user_profile_uuid": get_user_uuid(request),
-        "method": request.method,
-        "visitor_uuid": visitor_uuid
+        "title": get_response_title(response) or '',
+        "user_profile_uuid": get_user_uuid(request) or '',
+        "method": request.method or '',
+        "visitor_uuid": visitor_uuid or ''
     })
