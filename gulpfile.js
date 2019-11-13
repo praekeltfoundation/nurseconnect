d2305ad3a5f595fe4b6c438bff129adace481887
Mitso Qalaba <mqalaba@gmail.com>
2019-11-13 11:20:11 +0200

    update website styles

--- gulpfile.js
+++ gulpfile.js
@@ -19,9 +19,6 @@ var bourbon         = require('bourbon').includePaths;
 var uglify          = require('gulp-uglify');
 var concat          = require('gulp-concat');
 
-/* =================================== */
-/* *** constants *** */
-
 var srcPath = 'nurseconnect/static/src';
 var distPath = 'nurseconnect/static/dist';
 var templatesPath = 'nurseconnect/templates';
@@ -29,112 +26,104 @@ var templatesPath = 'nurseconnect/templates';
 var production = argv.production >= 1;
 
 var sassConfig = {
-    includePaths: [
-        'node_modules/breakpoint-sass/stylesheets/'
-    ].concat(bourbon),
-    outputStyle: 'compressed'
+  includePaths: [
+    'node_modules/breakpoint-sass/stylesheets/'
+  ].concat(bourbon),
+  outputStyle: 'compressed'
 };
 
+/* ================JS=================== */
 
-/* =================================== */
-/* *** JS *** */
 gulp.task('scripts', function() {
-    return gulp.src(srcPath + '/js/**/*.js')
-        .pipe(concat('index.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(distPath + '/js'));
+  return gulp.src(srcPath + '/js/**/*.js')
+    .pipe(concat('index.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(distPath + '/js'));
 });
 
 
-/* =================================== */
-/* *** SASS *** */
-
-gulp.task('styles', ['clean-css', 'lint-sass'], function () {
-    return gulp.src(srcPath + '/sass/**/*.s+(a|c)ss')
-    .pipe(plumber())
-    .pipe(sassGlob())
-    .pipe(sass(sassConfig).on('error', sass.logError))
-    .pipe(bless())
-    .pipe(gulpif(production, autoprefixer({
-        browsers: [
-            'ie >= 8',
-            'android >= 2.3',
-            'iOS >= 6',
-            '> 0%'
-        ]
-    })))
-    .pipe(pixrem())
-    .pipe(cssNano())
-    .pipe(plumber.stop())
-    .pipe(gulp.dest(distPath + '/css'))
-    .pipe(browserSync.stream());
-});
+/* ==============SASS===================== */
 
 gulp.task('clean-css', function() {
-    return del(distPath + '/css');
+  return del(distPath + '/css');
 });
 
 gulp.task('lint-sass', function() {
-    return gulp.src(srcPath + '/**/*.s+(a|c)ss')
-        .pipe(sassLint())
-        .pipe(sassLint.format())
-        .pipe(sassLint.failOnError());
+  return gulp.src(srcPath + '/**/*.s+(a|c)ss')
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError());
+});
+gulp.task('styles', gulp.series('clean-css', function () {
+  return gulp.src(srcPath + '/sass/**/*.s+(a|c)ss')
+  .pipe(plumber())
+  .pipe(sassGlob())
+  .pipe(sass(sassConfig).on('error', sass.logError))
+  .pipe(bless())
+  .pipe(gulpif(production, autoprefixer({
+    browsers: [
+        'ie >= 8',
+        'android >= 2.3',
+        'iOS >= 6',
+        '> 0%'
+    ]
+  })))
+  .pipe(pixrem())
+  .pipe(cssNano())
+  .pipe(plumber.stop())
+  .pipe(gulp.dest(distPath + '/css'))
+  .pipe(browserSync.stream());
+}));
+
+
+
+/* ================Generate Iconset=================== */
+
+gulp.task('clean-generated-icons', function() {
+  return del(srcPath + '/images/generated-icons');
 });
 
-/* =================================== */
-/* *** Watch *** */
+gulp.task('crush-svgs', gulp.series('clean-generated-icons', function () {
+  return gulp.src(srcPath + '/images/svgs/*.svg')
+    .pipe(svgmin())
+    .pipe(gulp.dest(srcPath + '/images/generated-icons'));
+}));
+
+gulp.task('clean-icons', function() {
+  return del(distPath + '/icons');
+});
+
+gulp.task('icons', gulp.series('clean-icons', 'crush-svgs', function (done) {
+  var icons = glob.sync(srcPath + '/images/generated-icons/*.*');
+  var options = {
+    dynamicColorOnly: true,
+    colors: {
+        orangeBittersweet: '#ff6655',
+        bluePelorous: '#2d9ec5',
+        blueRegal: '#213d55',
+        white: '#ffffff',
+        black: '#000000'
+    }
+  };
+}));
+/* ==============Static server===================== */
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      'proxy': 'localhost:8000/'
+  });
+  gulp.watch(srcPath + '/**/*.s+(a|c)ss', ['styles']);
+  gulp.watch(templatesPath + '/**/*.html').on('change', browserSync.reload);
+});
+
+
+/* ================Watch=================== */
+
 gulp.task('watch', function() {
     gulp.watch(srcPath + '/js/**/*.js', ['scripts']);
     gulp.watch(srcPath + '/sass/**/*.s+(a|c)ss', ['styles']);
 });
 
-/* =================================== */
-/* *** generate iconset *** */
+/* ================Default=================== */
 
-gulp.task('clean-generated-icons', function() {
-    return del(srcPath + '/images/generated-icons');
-});
-
-gulp.task('clean-icons', function() {
-    return del(distPath + '/icons');
-});
-
-gulp.task('crush-svgs', ['clean-generated-icons'], function () {
-    return gulp.src(srcPath + '/images/svgs/*.svg')
-        .pipe(svgmin())
-        .pipe(gulp.dest(srcPath + '/images/generated-icons'));
-});
-
-gulp.task('icons', ['clean-icons', 'crush-svgs'], function (done) {
-    var icons = glob.sync(srcPath + '/images/generated-icons/*.*');
-    var options = {
-        dynamicColorOnly: true,
-        colors: {
-            orangeBittersweet: '#ff6655',
-            bluePelorous: '#2d9ec5',
-            blueRegal: '#213d55',
-            white: '#ffffff',
-            black: '#000000'
-        }
-    };
-});
-/* =================================== */
-/* *** sync browser *** */
-
-
-// Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        'proxy': 'localhost:8000/'
-    });
-
-    gulp.watch(srcPath + '/**/*.s+(a|c)ss', ['styles']);
-    gulp.watch(templatesPath + '/**/*.html').on('change', browserSync.reload);
-});
-
-/* =================================== */
-/* *** Default tasks *** */
-
-gulp.task('default', ['clean-css'], function() {
-    runSequence('styles','scripts');
-});
+gulp.task('default', gulp.series('styles','scripts','icons'));
